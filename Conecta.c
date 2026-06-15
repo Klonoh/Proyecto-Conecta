01a8bc20ca5d439e6fa3b441e4ebe88313ad473e
Klonoh <toquin2003@gmail.com>
2026-06-15 13:48:14 -0400

    Se agrega un menú principal con opciones para interactuar con la aplicacion, como ver el feed, publicar mensajes, buscar usuarios, ver notificaciones, ver el perfil del usuario, obtener sugerencias y cerrar sesión.

--- Conecta.c
+++ Conecta.c
@@ -1,8 +1,105 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <limits.h>
 # include <string.h>
 # include "TDAs/list.h"
 # include "TDAs/map.h"
 # include "TDAs/queue.h"
 # include "TDAs/stack.h"
 # include "TDAs/extra.h"
+
+void mostrarMenuInicial(){
+  limpiarPantalla();
+  puts("========================================");
+  puts("     Conecta");
+  puts("========================================");
+
+  puts("1) Iniciar Sesión");
+  puts("2) Registrar Usuario");
+  puts("3) Salir");
+}
+void mostrarMenuPrincipal(){
+  limpiarPantalla();
+  puts("========================================");
+  puts("     Conecta");
+  puts("========================================");
+
+  puts("1) Ver feed");
+  puts("2) Publicar mensaje");
+  puts("3) Buscar usuario");
+  puts("4) Ver notificaciones");
+  puts("5) Ver mi perfil");
+  puts("6) Sugerencias para ti");
+  puts("7) Cerrar sesión");
+  puts("8) Salir");
+}
+
+void mostrarMenuPrincipal(){
+  limpiarPantalla();
+  puts("========================================");
+  puts("     Conecta");
+  puts("========================================");
+
+  puts("1) Cargar películas");
+  puts("2) Buscar por id");
+  puts("3) Buscar por director");
+  puts("4) Buscar por género");
+  puts("5) Buscar por década");
+  puts("6) Buscar por rango de calificaciones");
+  puts("7) Buscar por década y género");
+  puts("8) Gestionar watchlist");
+  puts("9) Salir");
+}
+
+
+int main(){
+
+    char opcion; 
+    char opcion_inicial;
+    do{
+        mostrarMenuInicial();
+        printf("Ingrese su opción: ");
+        scanf(" %c", &opcion_inicial);
+    
+        switch (opcion_inicial) {
+        case '1':
+        break;
+        case '2':
+        break;
+        case '3':
+        printf("Saliendo...\n");
+        return 0;
+        default:
+        printf("Opción inválida.\n");
+        break;
+        }
+        presioneTeclaParaContinuar();
+    } while (opcion_inicial != '3');
+
+
+    do {
+        mostrarMenuPrincipal();
+        printf("Ingrese su opción: ");
+        scanf(" %c", &opcion);
+
+        switch (opcion) {
+        case '1':
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
+        case '7':
+            break;
+        }
+        presioneTeclaParaContinuar();
+
+  } while (opcion != '8');
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Conecta
+## Descripción
+
+Aplicación desarrollada en lenguaje C para simular el funcionamiento de una red social al estilo Twitter/X
+## Requisitos
+
+* Compilador C (gcc recomendado)
+* Sistema operativo Linux, Mac o Windows (con MinGW)
+
+## Compilación
+
+### Windows (MinGW)
+```
+gcc Conecta.c TDAs/*.c -I TDAs -Wno-unused-result -o Conecta
+```
+
+## Ejecución
+
+```
+./Conecta
+```
