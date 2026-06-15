99f65c443e773030becdf662613f83d9e6218143
Klonoh <toquin2003@gmail.com>
2026-06-15 12:42:56 -0400

    Se incluyen las librerias, archivo .c esta listo para trabajar

---
+++ Conecta.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include "TDAs/list.h"
+# include "TDAs/map.h"
+# include "TDAs/queue.h"
+# include "TDAs/stack.h"
+# include "TDAs/extra.h"
